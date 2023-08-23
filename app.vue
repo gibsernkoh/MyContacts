8acc843d8cf7782d31d237c2413f3a5aa5deb24e
gibsernkoh <46896495+gibsernkoh@users.noreply.github.com>
2023-08-23 18:12:50 +0800

    WIP: Testing Supebase Connection

--- app.vue
+++ app.vue
@@ -2,4 +2,27 @@
   <div>
     <button class="btn">Testing</button>
   </div>
+  <pre>
+    Users:
+    {{ users }}
+  </pre>
 </template>
+
+<script setup>
+const supabase = useSupabaseClient();
+
+const users = ref([]);
+
+async function getUsers() {
+  //const res = await supabase.from('users').select();
+
+  console.log('_gib', res);
+
+  //users.value = res.data;
+}
+
+onMounted(() => {
+  console.log('_gib: mounted');
+  getUsers();
+});
+</script>
