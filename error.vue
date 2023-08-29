8450806ccf57bc28f354ee578def424a178f0644
gibsernkoh <gibsernkoh@gmail.com>
2023-08-30 00:33:47 +0800

    feat: add custom error page

---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="py-3 container mb-7 flex justify-between items-center">
+        <NuxtLink to="/">
+            <UILogo />
+        </NuxtLink>
+    </div>
+   
+    <div class="container">
+        <h1>Nothing to see here or is it..</h1>
+        <p class="text-lg">The page you are looking for is not found. <br/>Ohhh the sadness ... 😭</p>
+    </div>
+</template>
