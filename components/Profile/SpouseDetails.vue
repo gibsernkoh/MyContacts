339f740d754d9fd0977da39264dd35b58a5b9743
gibsernkoh <46896495+gibsernkoh@users.noreply.github.com>
2023-08-29 17:59:57 +0800

    WIP: started base ui for Spouse Details

--- components/Profile/SpouseDetails.vue
+++ components/Profile/SpouseDetails.vue
@@ -1,3 +1,51 @@
+<script setup>
+import { useSystemStore } from '~/store';
+
+defineProps({
+  toggleMode: {
+    type: Boolean,
+    required: true,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emits = defineEmits(['update:toggleMode', 'update:loading']);
+
+const system = useSystemStore();
+const client = useSupabaseClient();
+const user = useSupabaseUser();
+
+const { user_metadata } = user.value;
+
+const spouses = ref([]);
+</script>
+
 <template>
-    Spouse Details
+  <div class="overflow-x-auto">
+    <table class="table">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Salutation</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(spouse, index) in spouses" class="hover">
+          <th>{{ 1 + index }}</th>
+          <td>{{ spouse.salutation }}</td>
+          <td>{{ spouse.firstname }}</td>
+          <td>{{ spouse.lastname }}</td>
+          <td>
+            <!-- CTA -->
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
